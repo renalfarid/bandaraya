ce68665010da9db0b5b61a0983d9d73062126476
Zainal Farid <zainal@offeo.com>
2023-07-08 02:33:08 +0800

    menu link

--- assets/js/section.js
+++ assets/js/section.js
@@ -4,32 +4,32 @@ function loadSectionData() {
     .then(response => response.json())
     .then(data => {
         // Get the section data array from the JSON
-        var sectionData = data.section;
+        const sectionData = data.section;
         console.log('section data', sectionData);
 
         // Get the div element by its ID
-        var sectionDiv = document.getElementById('sectiondata');
+        let sectionDiv = document.getElementById('sectiondata');
 
         // Loop through the section data
-        for (var i = 1; i < sectionData.length; i += 4) {
-            var name = sectionData[i].name;
-            var content = sectionData[i + 1].content;
-            var image = sectionData[i + 2].image;
+        for (let i = 1; i < sectionData.length; i += 4) {
+            let name = sectionData[i].name;
+            let content = sectionData[i + 1].content;
+            let image = sectionData[i + 2].image;
 
             // Create a new section element
-            var section = document.createElement('section');
+            let section = document.createElement('section');
             section.id = name; // Use the name as the ID
 
             // Create a heading for the section
-            var heading = document.createElement('h2');
+            let heading = document.createElement('h2');
             heading.textContent = name;
 
             // Create a paragraph for the content
-            var paragraph = document.createElement('p');
+            let paragraph = document.createElement('p');
             paragraph.textContent = content;
 
             // Create an image element
-            var img = document.createElement('img');
+            let img = document.createElement('img');
             img.src = image;
 
             // Append the heading, paragraph, and image to the section
