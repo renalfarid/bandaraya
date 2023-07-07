95f0393180f3b2572c4d41e9208481429d35a343
Zainal Farid <zainal@offeo.com>
2023-07-08 03:42:03 +0800

    responsive

--- assets/js/promotion.js
+++ assets/js/promotion.js
@@ -8,16 +8,19 @@ function loadPromotionData() {
             // Get the promotion section and its elements by their IDs
             const promotionBase = document.getElementById('promotionBase');
             const promotionImage = document.getElementById('promotionImage');
+            const promotionOffer = document.getElementById('promotionOffer');
 
             // Set the data from the JSON to the HTML elements
             promotionBase.src = data.baseImage;
             promotionImage.src = data.image;
+            promotionOffer.src = data.offer;
 
             // Optional: You can add additional code to modify or style the elements as needed
 
             // Append the elements to the promotion section
             promotionSection.appendChild(promotionBase);
             promotionSection.appendChild(promotionImage);
+            promotionSection.appendChild(promotionOffer);
         })
         .catch(error => {
             console.log('Error fetching JSON:', error);
